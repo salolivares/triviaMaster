22a5b480a7cabadd5fa906adb7819191f1ad050b
Nivedita Choudhuri <nschoudhuri@engineering.ucsb.edu>
2015-01-31 18:47:08 -0800

    create account and database connection functions

--- src/edu/ucsb/cs48/util/User.java
+++ src/edu/ucsb/cs48/util/User.java
@@ -1,4 +1,4 @@
-package triviaMaster;
+package triviamaster;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,28 +11,82 @@ public class User {
 		boolean login = false;
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/triviamaster?" + "user=trivia&password=master");
-
+			Connection con = createDBconnection();
 			Statement stmt = (Statement) con.createStatement();
 			query = "SELECT username, password FROM user WHERE username='"
 					+ username + "' AND password='" + password + "';";
 			stmt.executeQuery(query);
 			ResultSet rs = stmt.getResultSet();
 			login = rs.first();
-
+			closeDBconnection(con);
 			return login;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 		return login;
 	}
 
-	public static void main(String[] args) {
-		if (loginCheck("nivedita","choudhuri"))
-			System.out.println("Success");
-		else
-			System.out.println("Login failure");
+	public static String createUser(String username, String password) {
+		try {
+			Connection con = createDBconnection();
+			Statement select_stmt = (Statement) con.createStatement();
+			String query = "SELECT username FROM user WHERE username='"
+					+ username + "';";
+			//System.out.println(query);
+			select_stmt.executeQuery(query);
+			ResultSet rs = select_stmt.getResultSet();
+			if (!(rs.first())) { /* If user does not exist */
+				String ins = "INSERT into user (username, password) values ('"
+						+ username + "','" + password + "');";
+				//System.out.println(ins);
+				Statement ins_stmt = (Statement) con.createStatement();
+				ins_stmt.executeUpdate(ins);
+			}
+			else { /* If user already exists */
+				return "DUPLICATE";
+			}
+			closeDBconnection(con);
+			return "CREATED";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "NOTCREATED";
 	}
+
+	public static Connection createDBconnection() {
+		String url = "jdbc:mysql://localhost/";
+		String dbName = "scot";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "scot";
+		String password = "tiger";
+		try {
+			Class.forName(driver).newInstance();
+			Connection conn = DriverManager.getConnection(url + dbName,
+					userName, password);
+
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void closeDBconnection(Connection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		if (loginCheck("nivedita", "choudhuri"))
+			System.out.println("loginCheck: Success");
+		else
+			System.out.println("loginCheck: Fail");
+		
+		String msg = createUser("user", "password");
+		System.out.println("createUser: " + msg);
+	}
+
 }
