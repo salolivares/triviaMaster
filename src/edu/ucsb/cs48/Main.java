bd15a3b1db161ff359066932654231734d6f3d62
Sal Olivares <salisonhismac@gmail.com>
2015-02-08 16:22:45 -0800

    made game backend run in seperate thread

--- src/edu/ucsb/cs48/Main.java
+++ src/edu/ucsb/cs48/Main.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs48;
 
 import edu.ucsb.cs48.gui.triviaMain;
 import edu.ucsb.cs48.util.Database;
+import edu.ucsb.cs48.util.Game;
 import edu.ucsb.cs48.util.User;
 
 /**
@@ -13,11 +14,15 @@ public class Main {
     // player is a static variable so it can be accessed by all other classes at runtime
     public static User player;
     public static Database db;
+    public static Game game;
+    public static Thread gameThread;
 
     public static void main(String[] args){
         triviaMain gui = new triviaMain();
         player = new User();
         db = new Database();
+        game = new Game();
+        gameThread = new Thread(game);
 
         gui.createWindow();
     }
