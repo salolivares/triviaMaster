536a78d290e70ffc9fa301ec949f87e033a7a7e0
Sal Olivares <salisonhismac@gmail.com>
2015-02-08 16:23:28 -0800

    made game a threadable class

--- src/edu/ucsb/cs48/util/Game.java
+++ src/edu/ucsb/cs48/util/Game.java
@@ -3,7 +3,9 @@ package edu.ucsb.cs48.util;
 import edu.ucsb.cs48.gui.countdownTimer;
 import edu.ucsb.cs48.gui.triviaMain;
 
-public class Game {
+import java.util.concurrent.TimeUnit;
+
+public class Game implements Runnable{
 
     // Unintialized constants used to choose questions
     // GUI should intialize these
@@ -13,17 +15,25 @@ public class Game {
     public static String CATEGORY = "null";
     public static int DIFFICULTY = 0;
 
-    public void start() throws InterruptedException {
+    public void run(){
+        try {
+            gameStart();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void gameStart() throws InterruptedException{
         countdownTimer();
     }
-    public void end(){
+    public void gameEnd(){
     }
     public void countdownTimer() throws InterruptedException{
         triviaMain.setCurrentPanel(new countdownTimer("3"));
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         triviaMain.setCurrentPanel(new countdownTimer("2"));
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         triviaMain.setCurrentPanel(new countdownTimer("1"));
-        Thread.sleep(2000);
+        Thread.sleep(1000);
     }
 }
