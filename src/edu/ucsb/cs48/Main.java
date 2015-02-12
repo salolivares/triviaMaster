2386f5112f038ca5b95740d923ff97c1ab0ab01f
Jordan Nguyen <jordannguyen@umail.ucsb.edu>
2015-02-11 21:32:34 -0800

    instantiated the QuestionAccess class

--- src/edu/ucsb/cs48/Main.java
+++ src/edu/ucsb/cs48/Main.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs48;
 
 import edu.ucsb.cs48.gui.triviaMain;
 import edu.ucsb.cs48.util.Database;
+import edu.ucsb.cs48.util.QuestionAccess;
 import edu.ucsb.cs48.util.User;
 
 /**
@@ -13,11 +14,13 @@ public class Main {
     // player is a static variable so it can be accessed by all other classes at runtime
     public static User player;
     public static Database db;
+    public static QuestionAccess qa;
 
     public static void main(String[] args){
         triviaMain gui = new triviaMain();
         player = new User();
         db = new Database();
+        qa = new QuestionAccess();
 
         gui.createWindow();
     }
