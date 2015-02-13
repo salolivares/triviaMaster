ac10411608fb3e646d5418ce6176993644bb6528
Jordan Nguyen <jordannguyen@umail.ucsb.edu>
2015-02-12 21:41:26 -0800

    added logic to check for correct answer

--- src/edu/ucsb/cs48/gui/gamePanel.java
+++ src/edu/ucsb/cs48/gui/gamePanel.java
@@ -22,15 +22,18 @@ public class gamePanel extends JPanel{
     JRadioButton answer4;
     JRadioButton answer5;
     JButton submit;
+    JButton next;
     Action pressedEnter;
+    //Action pressedEnterNext;
     Action pressed1;
     Action pressed2;
     Action pressed3;
     Action pressed4;
     Action pressed5;
+    String correctAnswer;
 
     public gamePanel(String q, String a1, String a2, String a3,
-                     String a4, String a5) {
+                     String a4, String a5,String cAnswer) {
 
         // Instantiate Components
         botPanel      = new JPanel(new GridBagLayout());
@@ -41,7 +44,10 @@ public class gamePanel extends JPanel{
         answer4       = new JRadioButton(a4);
         answer5       = new JRadioButton(a5);
         submit        = new JButton("Submit Answer");
+        next          = new JButton("Next");
+        next.setVisible(false);
         noAnsSelected = new JLabel("Please select an answer!");
+        correctAnswer = cAnswer;
         setLayout(new BorderLayout());
 
 
@@ -64,7 +70,7 @@ public class gamePanel extends JPanel{
 
 
         // Group the answers together so only one can be selected at a time
-        ButtonGroup group = new ButtonGroup();
+        final ButtonGroup group = new ButtonGroup();
         group.add(answer1);
         group.add(answer2);
         group.add(answer3);
@@ -100,6 +106,9 @@ public class gamePanel extends JPanel{
         botPanel.add(submit, gbc);
         gbc.gridx = 0;
         gbc.gridy = 7;
+        botPanel.add(next, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 8;
         botPanel.add(noAnsSelected, gbc);
 
         add(botPanel, BorderLayout.CENTER);
@@ -114,8 +123,7 @@ public class gamePanel extends JPanel{
                         && !(answer4.isSelected()) && !(answer5.isSelected()))
                     noAnsSelected.setVisible(true);
                 else {
-                    // gui signals to game controller that it is done
-                    Game.latch.countDown();
+                    checkAnswer();
                 }
 
             }
@@ -131,12 +139,20 @@ public class gamePanel extends JPanel{
                         && !(answer4.isSelected()) && !(answer5.isSelected()))
                     noAnsSelected.setVisible(true);
                 else {
-                    // gui signals to game controller that is is done
-                    Game.latch.countDown();
+                    checkAnswer();
                 }
             }
         });
 
+
+        next.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // gui signals to game controller that is is done
+                Game.latch.countDown();
+                }
+            }
+        );
 
         // Allow user to hit 1,2,3,4,5 to select an answer in addition to clicking enter
         pressed1 = new AbstractAction() {
@@ -194,7 +210,49 @@ public class gamePanel extends JPanel{
 
     }
 
-}
+    // A helper function to check if the user input the correct answer
+    public void checkAnswer() {
+        if (answer1.isSelected()) {
+            if (answer1.getText().equals(this.correctAnswer))
+                answer1.setForeground(Color.GREEN);
+            else
+                answer1.setForeground(Color.RED);
+            }
+        if (answer2.isSelected()) {
+            if (answer2.getText().equals(this.correctAnswer))
+                answer2.setForeground(Color.GREEN);
+            else
+                answer2.setForeground(Color.RED);
+            }
+        if (answer3.isSelected()) {
+            if (answer3.getText().equals(this.correctAnswer))
+                answer3.setForeground(Color.GREEN);
+            else
+                answer3.setForeground(Color.RED);
+            }
+        if (answer4.isSelected()) {
+            if (answer4.getText().equals(this.correctAnswer))
+                answer4.setForeground(Color.GREEN);
+            else
+                answer4.setForeground(Color.RED);
+            }
+        if (answer5.isSelected()) {
+            if (answer5.getText().equals(this.correctAnswer))
+                answer5.setForeground(Color.GREEN);
+            else
+                answer5.setForeground(Color.RED);
+            }
+
+        submit.setVisible(false);
+        next.setVisible(true);
+
+        }
+
+
+
+    }
+
+
 
 
 
