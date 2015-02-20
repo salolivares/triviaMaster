a904f28d1822a90b5ff48f1b287665b55d31c865
Jordan Nguyen <jordannguyen@umail.ucsb.edu>
2015-02-19 23:33:09 -0800

    added comments to countdownTimer

--- src/edu/ucsb/cs48/gui/countdownTimer.java
+++ src/edu/ucsb/cs48/gui/countdownTimer.java
@@ -3,10 +3,17 @@ package edu.ucsb.cs48.gui;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * countdownTimer class that creates a countdown prior to starting the game
+ */
 public class countdownTimer extends JPanel {
     //GUI Components
     JLabel numberLabel;
 
+    /**
+     * countdownTimer constructor
+     * @param number the string that will be displayed on the panel
+     */
     public countdownTimer(String number){
         numberLabel = new JLabel(number);
 
