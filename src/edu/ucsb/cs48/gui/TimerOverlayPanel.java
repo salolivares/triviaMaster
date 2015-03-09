7288e449f9442892ad981a59516117a8be0c6100
Sal Olivares <salisonhismac@gmail.com>
2015-03-08 22:33:09 -0700

    fix timer panel not adding points to user

--- src/edu/ucsb/cs48/gui/TimerOverlayPanel.java
+++ src/edu/ucsb/cs48/gui/TimerOverlayPanel.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs48.gui;
 
+import edu.ucsb.cs48.Main;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -28,6 +30,7 @@ public class TimerOverlayPanel extends JPanel implements Runnable{
     public void run() {
         setupTimerOverlay();
         runTimer();
+        Main.game.gameEnd();
         triviaMain.setCurrentPanel(new gameOverPanel());
     }
 
