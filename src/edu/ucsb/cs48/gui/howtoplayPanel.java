90d0ea7bde8842684f52a844dff57d446e190004
Brian Yan <brianyan@engineering.ucsb.edu>
2015-03-08 21:23:22 -0700

    added howtoplaypanel

--- src/edu/ucsb/cs48/gui/howtoplayPanel.java
+++ src/edu/ucsb/cs48/gui/howtoplayPanel.java
@@ -1,11 +1,9 @@
 package edu.ucsb.cs48.gui;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import edu.ucsb.cs48.Main;
 
 public class howtoplayPanel extends JPanel {
     JLabel title;
@@ -47,7 +45,7 @@ public class howtoplayPanel extends JPanel {
         //sets description
         description = new JLabel("<html> 1) Click Play Game <br/>" +
                 "2) Choose a game mode <br/>" +
-                "3) Next, choose a category and start game <br/>" +
+                "3) Next, pick a category and start game <br/>" +
                 "4) Use keys (1,2,3,4,5) to select an answer <br/>" +
                 "and press \"Enter\" to submit selection. <br/>" +
                 "</html>", JLabel.CENTER);
@@ -77,34 +75,6 @@ public class howtoplayPanel extends JPanel {
                 triviaMain.setCurrentPanel(new mainMenuPanel());
             }
         });
-//        description = new JLabel("Play this game");
-//        title = new JLabel("How To Play");
-//        background = new JLabel(new ImageIcon("assets/background.jpg"));
-//        playPanel = new JPanel();
-//        // sets the background
-//       // add(background);
-//        this.setLayout(null);
-//
-//        background = new JLabel(new ImageIcon("assets/background.jpg"));
-//        background.setOpaque(false);
-//        title.setFont(new Font("Courier New", Font.BOLD, 70));
-//        title.setVerticalAlignment(SwingConstants.CENTER);
-//        title.setHorizontalAlignment(SwingConstants.CENTER);
-//
-//        //back button
-//        backbutton.setFont(new Font("Courier New", Font.BOLD, 20));
-//        backbutton.setLocation(600,500);
-//        backbutton.setSize(75,25);
-//
-//        this.add(background);
-//        this.add(title);
-//
-//        backbutton.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseReleased(MouseEvent e) {
-//                triviaMain.setCurrentPanel(new mainMenuPanel());
-//            }
-//        });
-//        add(playPanel);
+
     }
 }
